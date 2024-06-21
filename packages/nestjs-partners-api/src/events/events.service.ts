9c761b413eeb80a34a7a152f694b6504e27ed60a
Felipe Basilio <felipe.basilio@avenuecode.com>
2024-06-21 01:10:40 -0300

    Created CRUD locally with Nest and paused class at 1:27:27

---
+++ packages/nestjs-partners-api/src/events/events.service.ts
@@ -0,0 +1,44 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+
+@Injectable()
+export class EventsService {
+  constructor(private PrismaService: PrismaService) {}
+
+  create(createEventDto: CreateEventDto) {
+    return this.PrismaService.event.create({
+      data: {
+        ...createEventDto,
+        date: new Date(createEventDto.date),
+      },
+    });
+  }
+
+  findAll() {
+    return this.PrismaService.event.findMany();
+  }
+
+  findOne(id: string) {
+    return this.PrismaService.event.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.PrismaService.event.update({
+      data: {
+        ...updateEventDto,
+        date: new Date(updateEventDto.date),
+      },
+      where: { id },
+    });
+  }
+
+  remove(id: string) {
+    return this.PrismaService.event.delete({
+      where: { id },
+    });
+  }
+}
